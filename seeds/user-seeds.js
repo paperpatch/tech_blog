24178cc2de98de226a5729494f63a77647bc1d63
paperpatch <patchen21@gmail.com>
2021-12-14 15:38:36 -0500

    removed related email items and changed respective to username

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,32 +4,26 @@ const { User, Post } = require('../models');
 const userdata = [
   {
     username: 'simple',
-    email: 'simple@simple.com',
     password: 'password123'
   },
   {
     username: 'random',
-    email: 'random@random.com',
     password: 'password123'
   },
   {
     username: 'secret',
-    email: 'secret@secret.com',
     password: 'password123'
   },
   {
     username: 'someone',
-    email: 'someone@someone.com',
     password: 'password123'
   },
   {
     username: 'food',
-    email: 'food@food.com',
     password: 'password123'
   },
   {
     username: 'mouse',
-    email: 'mouse@mouse.com',
     password: 'password123'
   },
 ];
