8ebf14d3da85d768c67f09af4bc5316fe72f7343
paperpatch <patchen21@gmail.com>
2021-12-14 14:50:21 -0500

    added login/signup features. fixed typos

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -97,7 +97,7 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/signup', (Req, res) => {
+router.get('/signup', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
     return;
