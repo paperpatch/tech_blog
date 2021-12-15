af26da5c02cb2bebada534d7a06b46cd964f4a60
paperpatch <patchen21@gmail.com>
2021-12-15 15:18:22 -0500

    comment out unused variables

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const req = require('express/lib/request');
-const sequelize = require('../config/connection');
+// const sequelize = require('../config/connection');
 const { Post, User, Comment} = require('../models');
 
 // get all posts for homepage
